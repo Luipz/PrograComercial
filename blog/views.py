7b9a3ba7d315422d860aee7567ae5be467e1c9e3
LuiPrz <joseluis12perez@mesoamericana.edu.gt>
2021-09-04 11:10:52 -0600

    estilo en Bulma

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from.models import Publicacion
 # Create your views here.
 def publicacion_lista(request):
     publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-    return render(request, 'blog/publicacion_lista.html', {'publicaciones': publicaciones})
-    #return render(request, 'blog/publicacion_listab.html', {'publicaciones': publicaciones})
+    #return render(request, 'blog/publicacion_lista.html', {'publicaciones': publicaciones})
+    return render(request, 'blog/publicacion_listab.html', {'publicaciones': publicaciones})
 
 def publicacion_detalle (request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
-    #return render(request, 'blog/publicacion_detalle.html',{'publicacion': publicacion})
+    return render(request, 'blog/publicacion_detalle.html',{'publicacion': publicacion})
