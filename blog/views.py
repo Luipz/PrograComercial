789b921656e4c686b69706300ff5a8b6797bb146
LuiPrz <joseluis12perez@mesoamericana.edu.gt>
2021-09-03 23:28:54 -0600

    Diseño en CSS

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,4 @@ def publicacion_lista(request):
 
 def publicacion_detalle (request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
-    return render(request, 'blog/publicacion_detalle.html',{'publicacion': publicacion})
+    #return render(request, 'blog/publicacion_detalle.html',{'publicacion': publicacion})
